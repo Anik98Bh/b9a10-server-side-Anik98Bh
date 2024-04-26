789598d4ea5f999c4aeb4a5af9ee7a3c6acc36dc
Anik98B <anikbhowmik0188@gmail.com>
2024-04-27 01:48:17 +0600

    get single data from server

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT||5000;
 
@@ -13,6 +13,7 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jgrphar.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,8 +27,29 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    await client.connect();
 
+    const touristsCollection = client.db("touristDB").collection('tourist');
+
+    app.get('/tourists', async (req, res) => {
+        const cursor = touristsCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    app.get('/tourists/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await touristsCollection.findOne(query);
+        res.send(result);
+    })
+
+    app.post('/tourists', async (req, res) => {
+        const newTourists = req.body;
+        console.log(newTourists);
+        const result = await touristsCollection.insertOne(newTourists);
+        res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
